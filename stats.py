5113d59144806293dfc9277abb532e1a5eba5150
Dominteanu Ionut Lucian <ionut.dominteanu@info.uaic.ro>
2018-11-26 13:40:06 +0200

    stats 7,7

--- stats.py
+++ stats.py
@@ -3,5 +3,5 @@ import hillclimbing_ai
 import random_ai
 import avg_calculator
 
-avg_calculator.calculate(6, 6, hillclimbing_ai.HillClimbingAI(None), "avg/hillclimb_ai - avg.txt", 1000)
-avg_calculator.calculate(6, 6, random_ai.RandomAI(None), "avg/random_ai - avg.txt", 1000)
+avg_calculator.calculate(7, 7, hillclimbing_ai.HillClimbingAI(None), "avg/hillclimb_ai - avg.txt", 1000)
+avg_calculator.calculate(7, 7, random_ai.RandomAI(None), "avg/random_ai - avg.txt", 1000)
