9bc10d3d6076ea4f30585565ba6f933fa1e264b6
narcisdiaconu <narcis.diaconu@info.uaic.ro>
2018-11-28 19:12:41 +0200

    A* reconstruct + testing

--- testAlgorithms.py
+++ testAlgorithms.py
@@ -1,7 +1,19 @@
 from iterativeDeepeningSearch import *
 from uniformCost import *
+import time
+
+instances = []
 
 file = open("instante.txt", 'r')
+
+for i in range(25):
+    inst = file.readline()
+    n = int(inst[0])
+    m = int(inst[2])
+    instances.append((n, m))
+
+file.close()
+
 file_write = open("./avg/ids_ai - avg.txt", 'w')
 
 def compute_height(n, m):
@@ -13,34 +25,50 @@ def compute_height(n, m):
         return ( 2 + m - n) * 4 - 1
     return 2*m-2
 
-for i in range(16):
-    instanta = file.readline()
-    n = int(instanta[0])
-    m = int(instanta[2])
+count_moves = 0
+count_time = 0
 
+for inst in instances:
+    n = inst[0]
+    m = inst[1]
     hanoi = Hanoi(n, m)
     ids = IterativeDeepeningSearch(hanoi, compute_height(n, m))
+    
+    start = time.perf_counter()
     result = ids.getResult()
-    string = "towers: " + str(n) + " disks: " + str(m) + '\n' + "len: " + str(result[1]) + "\n" + "number of states until reach final: " + str(result[2]) + "\n\n"
+    end = time.perf_counter() - start
+    
+    count_moves += result[2]
+    count_time += end
+
+    string = "towers: " + str(n) + " disks: " + str(m) + '\n' + "len: " + str(result[1]) + "\n" + "number of states until reach final: " + str(result[2]) + "\n" + "time: " + str(end) + " seconds" +"\n\n"
     print(string)
     file_write.write(string)
-
-file.close()
+string = "Average:" + "\n" + " - moves: " + str(count_moves / 25) + "\n" + " - time: " + str(count_time / 25)
+file_write.write(string)
 file_write.close()
 
-# file = open("instante.txt", 'r')
-# file_write = open("./avg/uniformcost_ai - avg.txt", 'w')
+file_write = open("./avg/uniformcost_ai - avg.txt", 'w')
 
-# for i in range(16):
-#     instanta = file.readline()
-#     n = int(instanta[0])
-#     m = int(instanta[2])
+count_moves = 0
+count_time = 0
 
-#     hanoi = Hanoi(n, m)
-#     uc = UniformCost(hanoi)
-#     uc.run()
-#     string = "towers: " + str(n) + " disks: " + str(m) + '\n' + "len: " + str(uc.result[0][1]) + "\n" + "number of states until reach final: " + str(uc.no_transitions) + "\n\n"
-#     print(string)
-#     file_write.write(string)
+for inst in instances:
+    n = inst[0]
+    m = inst[1]
 
-# file_write.close()
+    hanoi = Hanoi(n, m)
+    uc = UniformCost(hanoi)
+    
+    start = time.perf_counter()
+    uc.run()
+    end = time.perf_counter() - start
+    
+    count_moves += uc.no_transitions
+    count_time += end
+    string = "towers: " + str(n) + " disks: " + str(m) + '\n' + "len: " + str(uc.result[0][1]) + "\n" + "number of states until reach final: " + str(uc.no_transitions) + "\n" + "time: " + str(end) + " seconds" + "\n\n"
+    print(string)
+    file_write.write(string)
+string = "Average:" + "\n" + " - moves: " + str(count_moves / 25) + "\n" + " - time: " + str(count_time / 25)
+file_write.write(string)
+file_write.close()
--- uniformCost.py
+++ uniformCost.py
@@ -9,26 +9,24 @@ class UniformCost:
         self.result = 0
         self.tree = 0
         self.no_transitions = 0
+        self.history = [hanoi.state]
 
     def run(self):
         bfs_list = [(self.hanoi, 0)]
         while len(bfs_list) > 0:
             self.no_transitions +=1
-            accesible_state = getAccesibleStates(bfs_list[0][0], [])
+            accesible_state = getAccesibleStates(bfs_list[0][0], self.history)
             for state in accesible_state:
-                added = False
-                if state.state not in [i[0] for i in self.states]:
-                    self.states.append((state.state, bfs_list[0][1] + 1))
-                    added = True
+                self.states.append((state.state, bfs_list[0][1] + 1))
+                self.history.append(state.state)
                 if state.end():
                     self.result = [i for i in self.states if i[0] == state.state]
                     return state
-                if added == True:    
-                    bfs_list.append((state, bfs_list[0][1] + 1))
+                bfs_list.append((state, bfs_list[0][1] + 1))
             del bfs_list[0]
 
 if __name__ == "__main__":
-    h = Hanoi(4, 4, 1, 2)
+    h = Hanoi(4, 4)
     uc = UniformCost(h)
     uc.run()
     print(uc.result)
