7a6bff289c0d9b6bac063aea42017b1710734793
Dominteanu Ionut Lucian <ionut.dominteanu@info.uaic.ro>
2018-11-28 23:40:43 +0200

    instante

--- avg_calculator.py
+++ avg_calculator.py
@@ -8,10 +8,7 @@ class SampleStats:
         self.fails = fails
         self.duration = duration
 
-def average(towers_count, disks_count, ai, sample_size=1000, number_of_attempts=1000, tolerance=4):
-    hanoi = Hanoi(towers_count, disks_count)
-    ai.hanoi = hanoi
-
+def average(ai, sample_size=1000, number_of_attempts=1000, tolerance=4):
     moves_sum = 0
     attempts_sum = 0
     success_count = 0
@@ -19,38 +16,48 @@ def average(towers_count, disks_count, ai, sample_size=1000, number_of_attempts=
     start = time.perf_counter()
 
     for k in range(sample_size):
+        ai.hanoi.init()
         attempts = ai.solution(number_of_attempts=number_of_attempts, tolerance=tolerance)
         if attempts >= 0:
-            moves_sum += len(hanoi.moves_history)
+            moves_sum += len(ai.hanoi.moves_history)
             attempts_sum += attempts
             success_count += 1
 
-    duration = time.perf_counter() - start
+    duration = (time.perf_counter() - start) / sample_size
     avg_len = moves_sum / success_count if success_count > 0 else -1
     avg_attempts = attempts_sum / success_count if success_count > 0 else -1
     fails = sample_size - success_count
     return SampleStats(avg_len, avg_attempts, fails, duration)
 
 
-def calculate(towers_max_count, disks_max_count, ai, filename, sample_size=1000, number_of_attempts=1000, tolerance=4):
+def calculate(ai, read_filename, write_filename, sample_size=1000, number_of_attempts=1000, tolerance=4):
     big_string = "sample_size: {}  number_of_attempts: {}  tolerance: {}\n\n".format(
         sample_size, number_of_attempts, tolerance)
 
-    for i in range(3, towers_max_count + 1):
-        for j in range(3, disks_max_count + 1):
-            stats = average(i, j, ai, sample_size=sample_size, number_of_attempts=number_of_attempts)
+    with open(read_filename, "r") as fd:
+        instances = fd.readlines()
+
+    for instance in instances:
+        try:
+            towers_count, disks_count, pos_start, pos_end = [int(x) for x in instance.split(" ")]
+            ai.hanoi = Hanoi(towers_count, disks_count, pos_start, pos_end)
+
+            stats = average(ai, sample_size=sample_size, number_of_attempts=number_of_attempts)
 
             string = ""
-            string += "towers: {}  disks: {}\n".format(i, j)
+            string += "towers: {}  disks: {}  start: {}  end: {}\n".format(towers_count, disks_count, pos_start, pos_end)
             string += "avg length: {}\n".format(stats.avg_length)
             string += "avg attempts: {}\n".format(stats.avg_attempts)
-            string += "fails: {}\n".format(stats.fails)
-            string += "duration: {}\n\n".format(stats.duration)
+            string += "avg duration: {}\n".format(stats.duration)
+            string += "fails: {}\n\n".format(stats.fails)
 
             print(string)
             big_string += string
+        except:
+            print("invalid instance:\n {}".format(instance.split(" ")))
+            print()
 
 
-    fd = open(filename, "w")
+    fd = open(write_filename, "w")
     fd.write(big_string)
     fd.close()
--- stats.py
+++ stats.py
@@ -4,13 +4,5 @@ import random_ai
 import avg_calculator
 import time
 
-avg_calculator.calculate(6, 6, hillclimbing_ai.HillClimbingAI(None), "avg/hillclimb_ai - avg4.txt", sample_size=2000, number_of_attempts=100, tolerance=4)
-print("done")
-avg_calculator.calculate(6, 6, random_ai.RandomAI(None), "avg/random_ai - avg4.txt", sample_size=2000, number_of_attempts=100, tolerance=4)
-print("done")
-
-# ai = hillclimbing_ai.HillClimbingAI(None)
-# #ai = random_ai.RandomAI(None)
-# for i in range(3, 8): 
-#     metrics = avg_calculator.average(3, 6, ai, sample_size=100, number_of_attempts=100, tolerance=i)
-#     print("avg: {}  attempts: {}  fails: {}  time: {}".format(metrics.avg_length, metrics.avg_attempts, metrics.fails, metrics.duration))
+avg_calculator.calculate(hillclimbing_ai.HillClimbingAI(None), "instante - Copy.txt", "avg/hillclimb_ai - avg4.txt", sample_size=1000, number_of_attempts=100, tolerance=4)
+avg_calculator.calculate(random_ai.RandomAI(None), "instante - Copy.txt", "avg/random_ai - avg4.txt", sample_size=1000, number_of_attempts=100, tolerance=4)
