f52ef15a731a430895f277f072638a00fca164f3
Dominteanu Ionut Lucian <ionut.dominteanu@info.uaic.ro>
2018-11-28 09:58:54 +0200

    time counter

--- stats.py
+++ stats.py
@@ -2,10 +2,14 @@ from base import *
 import hillclimbing_ai
 import random_ai
 import avg_calculator
+import time
 
 # avg_calculator.calculate(7, 7, hillclimbing_ai.HillClimbingAI(None), "avg/hillclimb_ai - avg1.txt", 1000)
 # avg_calculator.calculate(7, 7, random_ai.RandomAI(None), "avg/random_ai - avg1.txt", 1000)
 
 ai = hillclimbing_ai.HillClimbingAI(None)
 #ai = random_ai.RandomAI(None)
-print(avg_calculator.average(5, 3, ai, sample_size=1000, number_of_attempts=2000, tolerance=8, return_fails=True))
+start = time.perf_counter()
+avg = avg_calculator.average(3, 6, ai, sample_size=100, number_of_attempts=500, tolerance=4, return_fails=True)
+elapsed = time.perf_counter() - start
+print("avg: {}  time: {}".format(avg, elapsed)) 
