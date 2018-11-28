7f0257134b1a9e93cefcb7d745f5da5aea17ec0d
Dominteanu Ionut Lucian <ionut.dominteanu@info.uaic.ro>
2018-11-28 11:13:48 +0200

    stats - time

--- avg_calculator.py
+++ avg_calculator.py
@@ -1,33 +1,42 @@
 from base import *
+import time
 
-def average(towers_count, disks_count, ai, sample_size=1000, number_of_attempts=1000, tolerance=4, return_fails=False):
+
+def average(towers_count, disks_count, ai, sample_size=1000, number_of_attempts=1000, tolerance=4):
     hanoi = Hanoi(towers_count, disks_count)
     ai.hanoi = hanoi
 
     moves_sum = 0
     success_count = 0
 
+    start = time.perf_counter()
+
     for k in range(sample_size):
-        result = ai.solution(number_of_attempts=number_of_attempts, tolerance=tolerance)
+        result = ai.solution(
+            number_of_attempts=number_of_attempts, tolerance=tolerance)
         if result:
             moves_sum += len(hanoi.moves_history)
             success_count += 1
-    
+
+    duration = time.perf_counter() - start
     avg = moves_sum / success_count if success_count > 0 else -1
     fails = sample_size - success_count
-    return (avg, fails) if return_fails else avg
+    return avg, fails, duration
+
 
 def calculate(towers_max_count, disks_max_count, ai, filename, sample_size=1000, number_of_attempts=1000, tolerance=4):
-    string = ""
+    string = "sample_size: {}  number_of_attempts: {}  tolerance: {}".format(
+        sample_size, number_of_attempts, tolerance)
 
-    for i in range(3, towers_max_count):
-        for j in range(3, disks_max_count):
-            avg, fails = average(i, j, ai, sample_size=sample_size, number_of_attempts=number_of_attempts, return_fails=True)
+    for i in range(3, towers_max_count + 1):
+        for j in range(3, disks_max_count + 1):
+            avg, fails, duration = average(i, j, ai, sample_size=sample_size, number_of_attempts=number_of_attempts)
 
-            string += "towers: " + str(i) + " disks: " + str(j) + "\n"
-            string += "avg: " + str(avg) + "\n"
-            string += "failed: " + str(fails) + "\n\n"
+            string += "towers: {}  disks: {}\n".format(i, j)
+            string += "avg: {}\n".format(avg)
+            string += "failed: {}\n".format(fails)
+            string += "duration: {}\n".format(duration)
 
-    fd = open(filename, "w")   
+    fd = open(filename, "w")
     fd.write(string)
     fd.close()
--- stats.py
+++ stats.py
@@ -4,12 +4,14 @@ import random_ai
 import avg_calculator
 import time
 
-# avg_calculator.calculate(7, 7, hillclimbing_ai.HillClimbingAI(None), "avg/hillclimb_ai - avg1.txt", 1000)
-# avg_calculator.calculate(7, 7, random_ai.RandomAI(None), "avg/random_ai - avg1.txt", 1000)
+avg_calculator.calculate(5, 5, hillclimbing_ai.HillClimbingAI(None), "avg/hillclimb_ai - avg1.txt", sample_size=100, number_of_attempts=100, tolerance=3)
+print("done")
+avg_calculator.calculate(5, 5, random_ai.RandomAI(None), "avg/random_ai - avg1.txt", sample_size=100, number_of_attempts=100, tolerance=3)
+print("done")
 
-ai = hillclimbing_ai.HillClimbingAI(None)
-#ai = random_ai.RandomAI(None)
-start = time.perf_counter()
-avg = avg_calculator.average(3, 3, ai, sample_size=100, number_of_attempts=500, tolerance=4, return_fails=True)
-elapsed = time.perf_counter() - start
-print("avg: {}  time: {}".format(avg, elapsed)) 
+# ai = hillclimbing_ai.HillClimbingAI(None)
+# #ai = random_ai.RandomAI(None)
+# start = time.perf_counter()
+# avg = avg_calculator.average(3, 3, ai, sample_size=100, number_of_attempts=500, tolerance=4, return_fails=True)
+# elapsed = time.perf_counter() - start
+# print("avg: {}  time: {}".format(avg, elapsed)) 
